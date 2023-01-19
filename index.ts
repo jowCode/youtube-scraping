eec83f723e419c9666aa0453d8849a2dedcd724b
cube <cube@cube>
2023-01-19 11:03:58 +0100

    wp

--- index.ts
+++ index.ts
@@ -7,18 +7,21 @@ function parse(html) {
 
     // perform queries
     const results = [];
-    cheerioHTML('#contents ytd-video-renderer').each((i, link) => {
-        results.push({
-            link: cheerioHTML(link).find('#thumbnail').attr('href'),
-            title: cheerioHTML(link).find('#video-title').text(),
-            channel: cheerioHTML(link).find('#text a').text(),
-            channel_link: cheerioHTML(link).find('#text a').attr('href'),
-            views: cheerioHTML(link).find('#metadata-line span').text(),
-            thumbnail: cheerioHTML(link).find('#thumbnail img').attr('src'),
 
-            release_date: cheerioHTML(link).find('#metadata-line span:nth-child(2)').text(),
+
+    cheerioHTML('#contents ytd-video-renderer').each((J: number, linkInner: Element) => {
+        results.push({
+            link: cheerioHTML(linkInner).find('#thumbnail').attr('href'),
+            title: cheerioHTML(linkInner).find('#video-title').text(),
+            channel: cheerioHTML(linkInner).find('#text a').text(),
+            channel_link: cheerioHTML(linkInner).find('#text a').attr('href'),
+            views: cheerioHTML(linkInner).find('#metadata-line span').text(),
+            thumbnail: cheerioHTML(linkInner).find('#thumbnail img').attr('src'),
+            release_date: cheerioHTML(linkInner).find('#metadata-line span:nth-child(2)').text(),
         })
-    });
+
+    })
+
 
     const cleaned = [];
     for (let i = 0; i < results.length; i++) {
@@ -27,7 +30,12 @@ function parse(html) {
         if (res.link && res.link.trim() && res.title && res.title.trim()) {
             res.title = res.title.trim();
             res.rank = i + 1;
-            res.views = +res.views.substr(0, res.views.indexOf(' '));
+            const views = +res.views.substr(0, res.views.indexOf(' '));
+            if (isNaN(views))  {
+                res.views = 0;
+            } else {
+                res.views = views;
+            }
 
             cleaned.push(res);
         }
@@ -37,58 +45,46 @@ function parse(html) {
 }
 
 
-async function scroll(
-    page,
-    scrollDelay = 800,
-) {
-    try {
-        let previousHeight;
-
-        for (let i = 0; i < 9; i++) {
-            previousHeight = await page.evaluate('document.body.scrollHeight');
-            console.log('previousHeight: ', previousHeight);
-            await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-            await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
-            await page.waitForTimeout(scrollDelay);
-        }
-    } catch(e) { }
-    return page;
-}
-
-
 try {
     (async () => {
-        let results = {};
-        const browser = await puppeteer.launch({headless: false});
+        let results: {};
+        const browser = await puppeteer.launch({headless: true});
         const page = await browser.newPage();
         await page.goto('https://www.youtube.com');
-        await new Promise(r => setTimeout(r, 5000));
-        console.log('initial page loaded')
-        // await page.waitForSelector('tp-yt-paper-dialog, ytd-button-renderer', {timeout: 10000});
+        await new Promise(r => setTimeout(r, 4000));
+
         await page.keyboard.press('Tab');
-        await new Promise(r => setTimeout(r, 800));
+        await new Promise(r => setTimeout(r, 400));
         await page.keyboard.press('Tab');
-        await new Promise(r => setTimeout(r, 900));
+        await new Promise(r => setTimeout(r, 400));
         await page.keyboard.press('Tab');
-        await new Promise(r => setTimeout(r, 800));
+        await new Promise(r => setTimeout(r, 400));
         await page.keyboard.press('Tab');
-        await new Promise(r => setTimeout(r, 800));
+        await new Promise(r => setTimeout(r, 400));
         await page.keyboard.press('Tab');
-        await new Promise(r => setTimeout(r, 1000));
+        await new Promise(r => setTimeout(r, 400));
         await page.keyboard.press('Enter');
-        console.log('accepted consent banner');
-        await new Promise(r => setTimeout(r, 5000));
-        console.log('do the search');
-        await page.goto('https://www.youtube.com/results?search_query=a4+b5&sp=CAISBggEEAEYAw%253D%253D');
-        await page.waitForSelector('ytd-video-renderer,ytd-grid-video-renderer', {timeout: 10000});
-        console.log('start scrolling');
-        await scroll(page);
-        console.log('end scrolling');
+
+        await new Promise(r => setTimeout(r, 4000));
+
+        await page.goto('');
+        await page.waitForSelector('ytd-video-renderer, ytd-item-section-renderer, ytd-grid-video-renderer', {timeout: 10000});
+
+        for (let i = 0; i < 9; i++) {
+            await page.keyboard.press('End');
+            await new Promise(r => setTimeout(r, 2000));
+        }
+
+        for (let i = 0; i < 9; i++) {
+            await page.keyboard.press('Home');
+            await new Promise(r => setTimeout(r, 1000));
+        }
+
 
         let html = await page.content();
         results = parse(html);
         console.dir(results, {depth: null, colors: true});
-        // await browser.close();
+        await browser.close();
 
     })()
 } catch (err) {
